ab588e36c43292627a666368d8131c99caa18f58
hernangit config --global user.name John <hernan0066@gmail.com>
2021-11-09 18:09:41 -0300

    2.5 Disparar accion login

---
+++ src/actions/auth.js
@@ -0,0 +1,7 @@
+
+
+export const starLogin = (email, password)=>{
+    return async()=>{
+        console.log(email,password)
+    }
+}
--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,33 +1,61 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { starLogin } from "../../actions/auth";
+import { useForm } from "../../hook/useForm";
 
 
 const LoginScreen = () => {
+  
+  const  [formLoginValues, handleLoginInputChnage] = useForm({
+      lEmail: "coreo@coreo.com",
+      lPassword: "123456"
+  })  
+
+  const dispatch = useDispatch();
+
+  const { lEmail, lPassword } = formLoginValues;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    //console.log(formLoginValues);
+    dispatch(starLogin(lEmail, lPassword));
+
+
+  }
+
+  
   return (
     <div className="auth__main">
       <div className="auth__box-container">
         <h3 className="auth__title">Login</h3>
         
-        <form>
+        <form onSubmit= {handleLogin}>
           <input
             type="text"
             placeholder="email"
-            name="email"
+            name="lEmail"
             className="auth__input"
             autoComplete="off"
+            value={lEmail}
+            onChange={handleLoginInputChnage}
+            
           />
           <input
             type="password"
             placeholder="Password"
-            name="password"
+            name="lPassword"
             className="auth__input"
+            
+            value={lPassword}
+            onChange={handleLoginInputChnage}
           />
           <button type="submit" className="auth__btn btn-danger btn-block">
             Login
           </button>
           <div>
 
-          <span>Not a member?</span> <Link to="/register" className="link">Sign up now</Link><span> <i class="fas fa-arrow-right"></i></span>
+          <span>Not a member?</span> <Link to="/register" className="link">Sign up now</Link><span> <i className="fas fa-arrow-right"></i></span>
           </div>
         </form>
       </div>
