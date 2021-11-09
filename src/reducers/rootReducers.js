b8af6e95213331e40149cadb120e77f81b60bcdf
hernangit config --global user.name John <hernan0066@gmail.com>
2021-11-09 11:31:32 -0300

    2.4 authReducer

--- src/reducers/rootReducers.js
+++ src/reducers/rootReducers.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { calendaReducer } from "./calendaReducer";
 import {uiReducer} from "./uiReducer";
+import {authReducer} from "./authReducer";
 
 
 export const rootReducer = combineReducers({
     ui: uiReducer,
     calendar: calendaReducer,
+    auth: authReducer,
 });
